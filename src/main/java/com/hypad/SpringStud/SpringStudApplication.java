76077c48d1d287528b0d89bd2c351dbd6f24a3bd
hypad590 <aloxaryabov@gmail.com>
2024-05-23 16:37:17 +0300

    Init

---
+++ src/main/java/com/hypad/SpringStud/SpringStudApplication.java
@@ -0,0 +1,13 @@
+package com.hypad.SpringStud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hypad/SpringStud/SpringStudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hypad.SpringStud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
